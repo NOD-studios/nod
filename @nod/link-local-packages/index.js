39f74e2c5bdd91ea13d18ea10317f03c970769c7
Furkan Tunalı <mail@furkantunali.com>
2017-11-16 00:45:08 +0100

    improve linking

--- @nod/link-local-packages/index.js
+++ @nod/link-local-packages/index.js
@@ -24,12 +24,10 @@ const getDependencies = (path = cwdPath, packagePath = cwdPath) => {
     .assign(peerDependencies || {}, devDependencies || {}, dependencies || {})
 }
 
-const thrower = error => { throw error }
-
 const npmLoad = config => new Promise((resolve, reject) =>
   npm.load(config, resolve))
 
-const npmLink = (packageToLink = []) => new Promise((resolve, reject) =>
+const npmLink = (packageToLink = '') => new Promise((resolve, reject) =>
   npm.commands.link(packageToLink, resolve))
 
 const dependenciesWithLocalFolders = (localPath = cwdPath, packagePath = cwdPath) =>
@@ -46,33 +44,23 @@ const dependenciesWithLocalFolders = (localPath = cwdPath, packagePath = cwdPath
       found,
     ][1])
 
-const linkLocalPackages = (localPath = cwdPath, packagePath = cwdPath) =>
-  Promise
-    .all(dependenciesWithLocalFolders(localPath, packagePath)
-      .map(modulePath => [
-        debug('loading npm %O', {modulePath}),
-        npmLoad()
-          .then(() => [
-            debug('linking %O', {modulePath}),
-            npmLink([modulePath]),
-          ][1])
-          .then(() => [
-            debug('linked %O', {modulePath}),
-            modulePath
-          ][1])
-      ][1]))
-    .then(linkedModules => [
-      debug('linkedModules %O', {linkedModules}),
-      linkedModules,
-    ][1])
+const linkLocalPackages = (localPath = cwdPath, packagePath = cwdPath) => [
+    debug('linking packages %O', {localPath, packagePath}),
+    npmLink(dependenciesWithLocalFolders(localPath, packagePath)),
+    debug('linked packages %O', {localPath, packagePath}),
+  ][1]
 
 const run = (localPath = cwdPath, packagePath = cwdPath) =>
   typeof localPath !== 'string'
-    ? thrower(new Error('local path must be string'))
+    ? Promise.reject(new Error('local path must be string'))
     : [
       debug('localPath', localPath),
       debug('packagePath', packagePath),
-      linkLocalPackages(resolve(localPath), resolve(packagePath))
+      npmLoad()
+        .then(() => [
+          debug('npm loaded'),
+          linkLocalPackages(resolve(localPath), resolve(packagePath))
+        ][1])
     ][2]
 
 module.exports = run
--- @nod/link-local-packages/index.test.js
+++ @nod/link-local-packages/index.test.js
@@ -6,7 +6,7 @@ const { join } = require('path')
 
 test('links properly', t =>
   linkLocalPackages(join(__dirname, 'mock'))
-    .then(packages => t.is(packages instanceof Array, true)))
+    .then(packages => t.is(typeof packages, 'object')))
 
 test('linked package exists', t =>
   moduleExists(DUMMY_MODULE) && t.pass())
